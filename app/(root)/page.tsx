511b3fda86dff3a849a3b4a11a8e57a31100852c
kavinesh <kavineshkavinesh860@gmail.com>
2025-06-29 07:59:36 +0530

    Implement home filters

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import HomeFilter from "@/components/filter/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
@@ -8,10 +9,13 @@ import React from "react";
 const questions = [
   {
     _id: "1",
-    title: "What is Next.js?",
+    title: "What is React.js?",
     description:
       "Next.js is a React framework for building server-side rendered applications.",
-    tags: ["react", "nextjs", "javascript"],
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "Javascript" },
+    ],
     author: {
       _id: "123",
       name: "John Doe",
@@ -21,10 +25,13 @@ const questions = [
   },
   {
     _id: "2",
-    title: "How to use React hooks?",
+    title: "How to use Javascript hooks?",
     description:
       "React hooks are functions that let you use state and other React features without writing a class.",
-    tags: ["react", "hooks", "javascript"],
+    tags: [
+      { _id: "1", name: "Javascript" },
+      { _id: "2", name: "React" },
+    ],
     author: {
       _id: "456",
       name: "Jane Smith",
@@ -39,11 +46,19 @@ interface SearchParamsProps {
 }
 
 export default async function Home({ searchParams }: SearchParamsProps) {
-  const { query = "" } = await searchParams;
+  const { query = "", filter = "" } = await searchParams;
 
-  const filteredQuestions = questions.filter((question) =>
-    question.title.toLowerCase().includes(query?.toLowerCase())
-  );
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query?.toLowerCase());
+
+    const matchesFilter = filter
+      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
+      : true;
+
+    return matchesQuery && matchesFilter;
+  });
 
   return (
     <>
@@ -65,6 +80,7 @@ export default async function Home({ searchParams }: SearchParamsProps) {
           otherClasses="flex-1"
         />
       </section>
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
           <h1 key={question._id}>{question.title}</h1>
