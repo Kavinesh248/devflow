eb165d0170ec7702922fde00a1592cc898dafc7e
kavinesh <kavineshkavinesh860@gmail.com>
2025-07-06 10:11:30 +0530

    Implement crud apis

---
+++ app/database.ts
@@ -0,0 +1,54 @@
+import { de } from "zod/v4/locales";
+
+const tickets = [
+  {
+    id: 1,
+    name: "Fix Next.js API route bug",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 2,
+    name: "Implement new feature for user profiles",
+    status: "in-progress",
+    type: "feature",
+  },
+  {
+    id: 3,
+    name: "Update documentation for API endpoints",
+    status: "closed",
+    type: "documentation",
+  },
+  {
+    id: 4,
+    name: "Refactor codebase for better performance",
+    status: "open",
+    type: "refactor",
+  },
+  {
+    id: 5,
+    name: "Create unit tests for new components",
+    status: "in-progress",
+    type: "testing",
+  },
+  {
+    id: 6,
+    name: "Design new UI for dashboard",
+    status: "open",
+    type: "design",
+  },
+  {
+    id: 7,
+    name: "Fix CSS issues in mobile view",
+    status: "closed",
+    type: "bug",
+  },
+  {
+    id: 8,
+    name: "Add new analytics features to the app",
+    status: "in-progress",
+    type: "feature",
+  },
+];
+
+export default tickets;
