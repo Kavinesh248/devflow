60fcf9e02715b43315a35359de49a38c5b443e1d
kavinesh <kavineshkavinesh860@gmail.com>
2025-07-09 12:02:05 +0530

    Implement account dynamic routes

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -1,9 +1,10 @@
-import User from "@/database/user.model";
+import { NextResponse } from "next/server";
+
+import Account from "@/database/account.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import { AccountSchema, UserSchema } from "@/lib/validations";
 import { dbConnect } from "@/lib/mongoose";
-import { UserSchema } from "@/lib/validations";
-import { NextResponse } from "next/server";
 
 // GET /api/users/[id]
 export async function GET(
@@ -11,15 +12,15 @@ export async function GET(
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
-  if (!id) throw new NotFoundError("User");
+  if (!id) throw new NotFoundError("Account");
 
   try {
     await dbConnect();
-    const user = await User.findById(id);
+    const account = await Account.findById(id);
 
-    if (!user) throw new NotFoundError("User");
+    if (!account) throw new NotFoundError("User");
 
-    return NextResponse.json({ success: true, data: user }, { status: 200 });
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
   } catch (error) {
     return handleError(error, "api") as APIErrorResponse;
   }
@@ -35,11 +36,11 @@ export async function DELETE(
 
   try {
     await dbConnect();
-    const user = await User.findByIdAndDelete(id);
+    const account = await Account.findByIdAndDelete(id);
 
-    if (!user) throw new NotFoundError("User");
+    if (!account) throw new NotFoundError("Account");
 
-    return NextResponse.json({ success: true, data: user }, { status: 200 });
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
   } catch (error) {
     return handleError(error, "api") as APIErrorResponse;
   }
@@ -51,21 +52,25 @@ export async function PUT(
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
-  if (!id) throw new NotFoundError("User");
+  if (!id) throw new NotFoundError("Account");
 
   try {
     await dbConnect();
     const body = await request.json();
-    const validatedData = UserSchema.partial().parse(body);
+    const validatedData = AccountSchema.partial().safeParse(body);
 
-    const updatedUser = await User.findByIdAndUpdate(id, validatedData, {
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const updatedAccount = await Account.findByIdAndUpdate(id, validatedData, {
       new: true,
     });
 
-    if (!updatedUser) throw new NotFoundError("User");
+    if (!updatedAccount) throw new NotFoundError("Account");
 
     return NextResponse.json(
-      { success: true, data: updatedUser },
+      { success: true, data: updatedAccount },
       { status: 200 }
     );
   } catch (error) {
